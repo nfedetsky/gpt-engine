a286fc99c6a4ff5e47b0c179fbed5acab536a719
Nicholas Fedetsky <nfedetski@yandex.ru>
2025-12-03 18:48:37 +0300

    T1_EX.03.Primitive

---
+++ pythonProject/theme1_exersice_03_primitive.py
@@ -0,0 +1,23 @@
+'''
+Решение для примитивного нейробота, который не умеет распознавать функции
+'''
+import os
+from os import makedirs
+from os import rename
+from shutil import copy
+from shutil import rmtree
+
+path = os.path.join(f"C:\TMP", "test_folder")
+makedirs(path)
+soursePath = f"C:\TMP\example.txt"
+destinationPath = path + f"\example.txt"
+renFile = path + f"\copied_example.txt"
+
+copy(soursePath, destinationPath)
+rename(destinationPath, renFile)
+
+try:
+    rmtree(path)
+except OSError as error:
+    print(f"Ошибка при удалении директории '{DesPath}': {error}")
+
